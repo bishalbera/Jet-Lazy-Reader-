bc7d5a3bb7873ba9d4fd61d7b269d1a71c6cdee6
Bishal <bishalbera2@gmail.com>
2023-04-29 19:40:41 +0530

    splash screen added

--- app/src/main/java/com/bishal/lazyreader/screens/splash/ReaderSplashScreen.kt
+++ app/src/main/java/com/bishal/lazyreader/screens/splash/ReaderSplashScreen.kt
@@ -28,6 +28,59 @@ import kotlinx.coroutines.delay
 @Composable
 fun ReaderSplashScreen(navController: NavController){
 
+    val scale = remember {
+        androidx.compose.animation.core.Animatable(0f)
+    }
+    LaunchedEffect(key1 = true,){
+        scale.animateTo(targetValue = 0.9f,
 
+            animationSpec = tween(durationMillis = 800,
+
+                easing = {
+                    OvershootInterpolator(8f)
+                        .getInterpolation(it)
+                }))
+        delay(2000L)
+    }
+
+
+
+    Surface(modifier = Modifier
+        .padding(15.dp)
+        .size(330.dp)
+        .scale(scale.value),
+        shape = CircleShape,
+        color = Color.White,
+        border = BorderStroke(width = 2.dp,
+            color = Color.LightGray)
+    ) {
+        Column(
+            modifier = Modifier.padding(1.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            ReaderLogo()
+            Spacer(modifier = Modifier.height(15.dp))
+            Text(text = "\"Read. Change. Yourself \"",
+                style = MaterialTheme.typography.headlineSmall,
+                color = Color.LightGray)
+
+
+
+        }
+
+
+    }
+
+
+}
+
+@Composable
+fun ReaderLogo(modifier: Modifier = Modifier) {
+    Text(text = "Lazy Reader",
+        modifier = modifier.padding(bottom = 16.dp),
+        style = MaterialTheme.typography.headlineSmall,
+        color = Color.Red.copy(alpha = 0.5f)
+    )
 
 }
