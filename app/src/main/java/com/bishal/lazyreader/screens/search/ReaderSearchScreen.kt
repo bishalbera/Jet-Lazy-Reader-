7c44b9256b0732433901ddf68aa936868242689f
Bishal <bishalbera2@gmail.com>
2023-05-05 19:57:48 +0530

    list of dummy books added in search screen

--- app/src/main/java/com/bishal/lazyreader/screens/search/ReaderSearchScreen.kt
+++ app/src/main/java/com/bishal/lazyreader/screens/search/ReaderSearchScreen.kt
@@ -5,31 +5,52 @@
 package com.bishal.lazyreader.screens.search
 
 import android.annotation.SuppressLint
+import android.util.Log
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import coil.compose.rememberAsyncImagePainter
 import com.bishal.lazyreader.components.InputField
 import com.bishal.lazyreader.components.ReaderAppBar
+import com.bishal.lazyreader.model.MBook
+import com.bishal.lazyreader.navigation.ReaderScreen
 
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
@@ -53,8 +74,12 @@ fun ReaderSearchScreen(navController: NavController) {
               SearchForm(
                   modifier = Modifier
                       .fillMaxWidth()
-                      .padding(top = 16.dp)
-              )
+                      .padding(top = 16.dp)){
+                  Log.d("TAG", "Searchscreen: $it")
+
+              }
+                Spacer(modifier = Modifier.height(13.dp))
+                BookList(navController)
 
             }
 
@@ -63,6 +88,58 @@ fun ReaderSearchScreen(navController: NavController) {
 
     }
 
+
+}
+
+@Composable
+fun BookList(navController: NavController) {
+    val listOfBooks = listOf(
+        MBook(id = "sdjhk", title = null, authors = "gakhskj", notes = null),
+        MBook(id = "sdjhk", title = null, authors = "gakhskj", notes = null),
+        MBook(id = "sdjhk", title = null, authors = "gakhskj", notes = null),
+        MBook(id = "sdjhk", title = null, authors = "gakhskj", notes = null),
+        MBook(id = "sdjhk", title = "oull", authors = "gakhskj", notes = null)
+    )
+
+    LazyColumn(modifier = Modifier.fillMaxSize()){
+        items(items = listOfBooks){ book ->
+            BookRow(book, navController)
+
+        }
+
+    }
+}
+
+@Composable
+fun BookRow(book: MBook,
+            navController: NavController) {
+    Card(modifier = Modifier
+        .clickable {
+            navController.navigate(ReaderScreen.DetailScreen.name + "/${book.id}")
+        }
+        .fillMaxWidth()
+        .height(100.dp)
+        .padding(3.dp),
+        shape = RectangleShape,
+        elevation = CardDefaults.cardElevation(7.dp)) {
+        Row(modifier = Modifier.padding(4.dp),
+            verticalAlignment = Alignment.Top) {
+            val imageUrl = "https://img.freepik.com/free-vector/hand-drawn-flat-design-stack-books-illustration_23-2149330605.jpg?w=2000"
+            Image(painter = rememberAsyncImagePainter(model = imageUrl),
+                contentDescription = "book",
+                modifier = Modifier.width(80.dp)
+                    .fillMaxHeight()
+                    .padding(end = 4.dp))
+            Column {
+                Text(text = book.title.toString(), overflow = TextOverflow.Ellipsis)
+                Text(text = "Author: ${book.authors}",
+                    overflow = TextOverflow.Clip,
+                    style = MaterialTheme.typography.labelSmall)
+            }
+
+        }
+
+    }
 
 }
 
