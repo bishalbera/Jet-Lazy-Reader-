2faf9c6a05f5231279a4d6b93d329f3b02fb52eb
Bishal <bishalbera2@gmail.com>
2023-05-16 17:13:25 +0530

    screen navigations are now animated

--- app/src/main/java/com/bishal/lazyreader/MainActivity.kt
+++ app/src/main/java/com/bishal/lazyreader/MainActivity.kt
@@ -8,7 +8,6 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
